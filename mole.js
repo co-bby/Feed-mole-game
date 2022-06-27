9ee9855c17b5de5adf2197d898c70cbdef367a14
co-bby <nkumsagoefredrick@gmail.com>
2022-06-26 23:28:36 -0400

    mole status

--- mole.js
+++ mole.js
@@ -1,1 +1,101 @@
+function getSadInterval() {
+  return Date.now() + 1000;
+}
 
+function getGoneInterval() {
+  return date.now() + Math.floor(Math.random() * 18000) + 2000;
+}
+
+const moles = [
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-0'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-1'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-2'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-3'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-4'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-5'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-6'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-7'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-8'),
+  },
+  {
+    status: 'sad',
+    next: getSadInterval(),
+    king: false,
+    node: document.querySelector('#hole-9'),
+  },
+];
+
+function getNextStatus(mole) {
+  switch (mole.status) {
+    case 'sad':
+      mole.next = getSadInterval();
+      mole.status = 'leaving';
+      mole.node.src = './Static/mole-leaving.png';
+      break;
+    case 'leaving':
+      mole.next = Date.now() + 1000;
+  }
+}
+
+let runAgainAt = Date.now() + 100;
+
+function nextFrame() {
+  const now = Date.now();
+
+  if (runAgainAt <= now) {
+    for (let i = 0; i < moles.length; i++) {
+      if (moles[i].next <= now) {
+        getNextStatus(moles[i]);
+      }
+    }
+    runAgainAt = now + 100;
+  }
+
+  requestAnimationFrame(nextFrame);
+}
+
+nextFrame();
